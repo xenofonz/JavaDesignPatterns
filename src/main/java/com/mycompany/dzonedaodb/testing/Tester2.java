78e3102997529190088ec44223bdb6d9c6c10fde
xenofonz <=fonn80@hotmail.com>
2018-03-20 15:21:33 +0200

    Add of Tester2.java class

---
+++ src/main/java/com/mycompany/dzonedaodb/testing/Tester2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dzonedaodb.testing;
+
+/**
+ *
+ * @author TENANTS\zinovioux
+ */
+public class Tester2 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Tester No2 working");
+    }
+    
+}
